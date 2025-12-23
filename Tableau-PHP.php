141ad3a1399a4f6b8a1ec4a9f043f8504f9e63a3
Réginald <mornet.reginald@gmail.com>
2025-12-23 16:32:04 +0100

    exo 2.4 en cours

--- Tableau-PHP.php
+++ Tableau-PHP.php
@@ -1,7 +1,7 @@
 <!-- Exercice 1.1 – Création et affichage -->
 
 <?php
-$fruits = [ "pomme","banane", "orange"];
+$fruits = ["pomme", "banane", "orange"];
 foreach ($fruits as $fruit) {
     echo $fruit . "<br>";
 }
@@ -11,18 +11,57 @@ foreach ($fruits as $fruit) {
 <!-- Exercice 1.2 – Accès par index -->
 
 <?php
-$fruits = ["pomme","banane", "orange"];
-echo $fruits [0]; 
+$fruits = ["pomme", "banane", "orange"];
+echo $fruits[0];
 echo "<br>";
-echo $fruits [2];
+echo $fruits[2];
 ?>
 <br>
 
 <!-- Exercice 1.3 – Ajout d’éléments -->
 
 <?php
-$fruits = ["pomme","banane", "orange"];
+$fruits = ["pomme", "banane", "orange"];
 array_push($fruits, "fraise", "framboise");
-echo $fruits;
-var_dump($fruits);
+foreach ($fruits as $fruit) {
+    echo $fruit . "<br>";
+}
 ?>
+<br>
+
+<!-- Exercice 1.4 – Compter les élément -->
+
+<?php
+$fruits = ["pomme", "banane", "orange"];
+$longueur = count($fruits);
+echo $longueur;
+?>
+<br>
+
+<!-- Exercice 2.1 – Création et lecture -->
+
+<?php
+$personnes = ["nom" => "Durand", "prenom" => "Alice", "age" => "28"];
+echo $personnes["prenom"] . " " . $personnes["nom"] . " " . "a" . " " . $personnes["age"] . " " . "ans";
+?>
+<br>
+
+<!-- Exercice 2.2 – Boucle sur tableau associatif -->
+
+<?php
+$personnes = ["nom" => "Durand", "prenom" => "Alice", "age" => "28"];
+foreach ($personnes as $cle => $valeur){
+echo $cle . " " . ":" . " " . $valeur;
+}
+?>
+<br>
+
+<!-- Exercice 2.3 – Modification de valeur -->
+
+<?php
+$personnes = ["nom" => "Durand", "prenom" => "Alice", "age" => "28"];
+$personnes ["age"] = 30;
+echo $personnes["prenom"] . " " . $personnes["nom"] . " " . "a" . " " . $personnes["age"] . " " . "ans";
+?>
+<br>
+
